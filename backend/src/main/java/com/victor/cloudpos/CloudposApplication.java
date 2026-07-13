d4fecebba718c2e955289af65293a09e1095ce21
VictorLeonDev <vrleon98@gmail.com>
2026-07-13 11:29:32 -0600

    chore(backend): initialize Spring Boot application

---
+++ backend/src/main/java/com/victor/cloudpos/CloudposApplication.java
@@ -0,0 +1,13 @@
+package com.victor.cloudpos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudposApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/victor/cloudpos/CloudposApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victor.cloudpos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudposApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
